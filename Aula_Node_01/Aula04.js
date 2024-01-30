d02c164ad0570ced5252d321861a16529ea8814a
Luiz Ricardo Filho <filho.ricardo13@gmail.com>
2024-01-30 16:44:10 -0300

    ultimo commit da aula

--- Aula_Node_01/Aula04.js
+++ Aula_Node_01/Aula04.js
@@ -14,5 +14,18 @@ const fctCalcVolume = (radius) => {
 reqEntrada.question("Qual o valor do raio que deseja calcular? ", radius => {
     console.log("O raio é: %d", radius);
 
+    let msgErro = "É necessário definir o valor numérico para poder efetuar a operação";
+
+    if(!radius){
+        console.log(msgErro);
+    }else {
+        if(isNaN(radius)){
+            console.log(msgErro);
+        }else {
+            console.log(`O diâmetro da esfera de raio ${radius} é ${fctCalcDiametro(radius)}, feito usando a função javaScript padrão.`);
+            console.log(`O volume de uma esfera de raio ${radius} é ${fctCalcDiametro(radius)}, feito usando a aroow functions que é uma simplificação para funções de expressões.`);
+        }
+    }
+
     reqEntrada.close();
 })
