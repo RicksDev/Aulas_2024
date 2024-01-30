7846af285aafcc6796df26954f4c19ddfa4e5625
Luiz Ricardo Filho <filho.ricardo13@gmail.com>
2024-01-30 16:00:37 -0300

    Atualizando dados

---
+++ Aula_Node_01/Aula02.js
@@ -0,0 +1,57 @@
+function fctTexto(){
+    let intV1 = 50;
+    let intV2 = 40;
+    let intV3 = intV1 + intV2;
+    let strNome = "Marcos";
+    
+
+    console.log ("Olá %s, o resultado da operação foi: %d", strNome, intV3);
+}
+
+fctTexto();
+
+ //Function sem paramêtro
+const fctMensagem = () => {
+    let nome = "Renato";
+    console.log("Tenha um excelente dia, %s", nome);
+}
+
+fctMensagem();
+
+//FUnction com paramêtro de retorno
+function fctCalculo(a,b){
+    let intDif = 0;
+    let intResult = a+b;
+    if (a > b){
+        intDif = a-b;
+    }else {
+        intDif = b-a;
+    }
+    for(let i=1; i <= intDif; i++){
+        intResult *= i;
+    }
+    return intResult;
+}
+console.log(fctCalculo(6,8));
+
+const fctFibonacci =(intValor) => {
+    let intTermo = intValor;
+    let intPenultimo = 0;
+    let intUltimo = 1;
+    let intNumero = 0;
+    let intCount = 3;
+k
+    while (intCount <= intTermo){
+        intNumero = intUltimo + intPenultimo;
+        intPenultimo = intUltimo;
+        intUltimo = intNumero;
+        intCount++;
+    }
+    return intNumero;
+}
+
+const reqEntrada = require('readline').createInterface({
+    input: process.stdin,
+    output:process.stdout
+});
+
